ce997e89a39294867f428fe53ea258c303428089
Todor Arnaudovski <todorarnaudovski@gmail.com>
2022-11-14 20:54:19 +0100

    added minor fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { ScrollToTop } from 'helpers';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ScrollToTop />
       <App />
     </BrowserRouter>
